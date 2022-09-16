1c9b26629fe9c14f44c7401af9dade653ba316e1
savoloon <nugaev.vlad@mail.ru>
2022-09-16 21:05:23 +0500

    #3 Добавили кнопку на странице.

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <h2>Спасибо, что зашли на мой github</h2>
+  <div>
+    <button @click="addClicks">Clicks</button>
+    <strong style="margin-left: 10px">{{ clicks }}</strong>
+  </div>
 </template>
 
 <script>
